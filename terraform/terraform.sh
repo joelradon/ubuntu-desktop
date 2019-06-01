2515d984783b349f87a9fcc0d7d268afcdca8bb2
joelradon <joelradon@hotmail.com>
2019-05-31 21:55:04 -0400

    init

---
+++ terraform/terraform.sh
@@ -0,0 +1,5 @@
+sudo apt install unzip
+sudo wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip
+sudo unzip terraform_0.12.0_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
--- vagrant/vagrant-qemu.sh
+++ vagrant/vagrant-qemu.sh
@@ -16,11 +16,8 @@ sudo vagrant plugin install vagrant-libvirt
 #***********************************************************
 # Download, copy, and install vagrant
 #***********************************************************
-sudo apt install unzip
-sudo wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip
-sudo unzip terraform_0.12.0_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
 
+sudo apt install vagrant
 #***********************************************************
 # Start Vagrant VM
 # In prepared project directory, run following command:
